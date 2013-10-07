6dca68fb2e6db7c0c9042c11294f772d9f0db3e8
morecowbellvt <chris.cerwinski@gmail.com>
2013-10-06 21:49:03 -0400

    added color palette, storyboard updates

---
+++ uLink/ULinkColorPalette.h
@@ -0,0 +1,39 @@
+//
+//  ULinkColorPalette.h
+//  ulink
+//
+//  Created by Christopher Cerwinski on 10/4/13.
+//  Copyright (c) 2013 uLink, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIColor.h>
+#import "ColorConverterUtil.h"
+
+@interface UIColor(ColorPalette)
++ (UIColor *)uLinkLightBlueGreenColor;
++ (UIColor *)uLinkBlueGreenColor;
++ (UIColor *)uLinkDarkBlueGreenColor;
++ (UIColor *)uLinkLightBlueColor;
++ (UIColor *)uLinkBlueColor;
++ (UIColor *)uLinkPurpleColor;
++ (UIColor *)uLinkDarkPurpleColor;
++ (UIColor *)uLinkDeepPurpleColor;
++ (UIColor *)uLinkLightPurpleColor;
++ (UIColor *)uLinkLightestPurpleColor;
++ (UIColor *)uLinkLightGreenColor;
++ (UIColor *)uLinkGreenColor;
++ (UIColor *)uLinkDarkGreenColor;
++ (UIColor *)uLinkSeaGreenColor;
++ (UIColor *)uLinkLightSeaGreenColor;
++ (UIColor *)uLinkOrangeColor;
++ (UIColor *)uLinkTanColor;
++ (UIColor *)uLinkDarkTanColor;
++ (UIColor *)uLinkLightOrangeColor;
++ (UIColor *)uLinkPeachColor;
++ (UIColor *)uLinkDarkGrayColor;
++ (UIColor *)uLinkGrayColor;
++ (UIColor *)uLinkLightGrayColor;
++ (UIColor *)ULinkWhiteColor;
+@end
+
---
+++ uLink/ULinkColorPalette.m
@@ -0,0 +1,108 @@
+//
+//  ULinkColorPalette.m
+//  ulink
+//
+//  Created by Christopher Cerwinski on 10/4/13.
+//  Copyright (c) 2013 uLink, Inc. All rights reserved.
+//
+
+#import "ULinkColorPalette.h"
+
+@implementation UIColor(ColorPalette)
+
++ (UIColor *)uLinkLightBlueGreenColor {
+    return [UIColor colorWithHexString:@"#378194"];
+}
+
++ (UIColor *)uLinkBlueGreenColor {
+    return [UIColor colorWithHexString:@"#3B656F"];
+}
+
++ (UIColor *)uLinkDarkBlueGreenColor {
+    return [UIColor colorWithHexString:@"#125160"];
+}
+
++ (UIColor *)uLinkLightBlueColor {
+    return [UIColor colorWithHexString:@"#84BCCA"];
+}
+
++ (UIColor *)uLinkBlueColor {
+    return [UIColor colorWithHexString:@"#6BB7CA"];
+}
+
++ (UIColor *)uLinkPurpleColor {
+    return [UIColor colorWithHexString:@"#465AA2"];
+}
+
++ (UIColor *)uLinkDarkPurpleColor {
+    return [UIColor colorWithHexString:@"#46517A"];
+}
+
++ (UIColor *)uLinkDeepPurpleColor {
+    return [UIColor colorWithHexString:@"#172869"];
+}
+
++ (UIColor *)uLinkLightPurpleColor {
+    return [UIColor colorWithHexString:@"#7788D1"];
+}
+
++ (UIColor *)uLinkLightestPurpleColor {
+    return [UIColor colorWithHexString:@"#8F9DD1"];
+}
+
++ (UIColor *)uLinkLightGreenColor {
+    return [UIColor colorWithHexString:@"#3FB35E"];
+}
+
++ (UIColor *)uLinkGreenColor {
+    return [UIColor colorWithHexString:@"#458656"];
+}
+
++ (UIColor *)uLinkDarkGreenColor {
+    return [UIColor colorWithHexString:@"#15742E"];
+}
+
++ (UIColor *)uLinkSeaGreenColor {
+    return [UIColor colorWithHexString:@"#70D98B"];
+}
+
++ (UIColor *)uLinkLightSeaGreenColor {
+    return [UIColor colorWithHexString:@"#8CD9A0"];
+}
+
++ (UIColor *)uLinkOrangeColor {
+    return [UIColor colorWithHexString:@"#EFA154"];
+}
+
++ (UIColor *)uLinkTanColor {
+    return [UIColor colorWithHexString:@"#B3885C"];
+}
+
++ (UIColor *)uLinkDarkTanColor {
+    return [UIColor colorWithHexString:@"#9B5B1B"];
+}
+
++ (UIColor *)uLinkLightOrangeColor {
+    return [UIColor colorWithHexString:@"#F7BB7F"];
+}
+
++ (UIColor *)uLinkPeachColor {
+    return [UIColor colorWithHexString:@"#F7CB9F"];
+}
+
++ (UIColor *)uLinkDarkGrayColor {
+    return [UIColor colorWithHexString:@"#878D8E"];
+}
+
++ (UIColor *)uLinkGrayColor {
+    return [UIColor colorWithHexString:@"#C1C6C7"];
+}
+
++ (UIColor *)uLinkLightGrayColor {
+    return [UIColor colorWithHexString:@"#E6E0DA"];
+}
+
++ (UIColor *)ULinkWhiteColor {
+    return [UIColor colorWithHexString:@"#F2EFEC"];
+}
+@end
